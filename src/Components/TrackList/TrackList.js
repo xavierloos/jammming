c7950dc05a38b93bafbeec40cd5d64cd1dafbe23
Javier Lopez <javierlopez@MacBook-Pro.home>
2020-08-23 17:40:16 +0100

    Adding Components

---
+++ src/Components/TrackList/TrackList.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './TrackList.css';
+
+function TrackList() {
+    return (
+        <div className="TrackList">
+            {/* <!-- You will add a map method that renders a set of Track components  --> */}
+        </div>
+    );
+}
+
+export default TrackList;
